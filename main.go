671e872e6acf1d1a275120565a56354771e7b3db
Kwangyoung Allen Kim <blackdog0403@gmail.com>
2019-12-18 16:27:22 +0900

    update message

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello Optank Enterprise"}
+		res := &response{Message: "Hello Coupang Folks!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
